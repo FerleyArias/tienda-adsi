321978764ae398f432dc4d864cc3ab8bb3d8494a
Ferley Arias <ferley225@gmail.com>
2021-03-06 15:47:21 -0500

    fix typo

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,13 +7,13 @@ const authentication = {
     const user = await User.findOne({email})
     if(!user || user.state === 0) {
       return res.status(400).json({
-        msg: 'Password o usuario inconrrectos'
+        msg: 'usuario o contraseña inconrrectos'
       })
     }
     const validationPass = bcrypt.compareSync(password,user.password) 
     if(!validationPass) {
       return res.status(400).json({
-        msg: 'Password o usuario inconrrectos'
+        msg: 'usuario o contraseña inconrrectos'
       })
     }
     res.json({
